0fd9f7781beef71002dd024b31d96687363de592
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-23 08:08:19 -0500

    ngStyle

--- course-practice/src/app/app.component.ts
+++ course-practice/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showText = false;
+  log = [];
+
+  onToggleText () {
+    this.showText = !this.showText;
+    this.log.push (this.log.length +1);
+  }
 }
