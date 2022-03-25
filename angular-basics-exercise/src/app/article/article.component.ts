d7cb22f169cb8005202de52b22c36421e1cae922
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-24 19:09:22 -0500

    bonus 12.7

--- angular-basics-exercise/src/app/article/article.component.ts
+++ angular-basics-exercise/src/app/article/article.component.ts
@@ -14,6 +14,7 @@ export class ArticleComponent implements OnInit {
   title: string = "This is the title.";
   content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  isTechRelated: boolean = false;
+  isTechRelated: boolean = true;
 
+  changeColor() {this.isTechRelated = !this.isTechRelated};
 }
