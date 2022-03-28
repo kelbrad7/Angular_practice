56b349634d7879106e046ba4e714e3a718fe562f
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-28 14:25:13 -0500

    event binding

---
+++ basic-event-binding-exercise/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-event-binding-exercise';
+  consoleBtn = () => console.log("Happy Birthday Molly Kate");
+}
