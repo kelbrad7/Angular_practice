fe42484221b837a836e669d5ef44b2f9f04e6efc
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-31 16:47:45 -0500

    exercise 3

---
+++ angular-ngFor-input-decorator-exercise/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   users: 
+    {
+      username: string,
+      id: number
+    } [] = 
+    [
+    { username: 'Kelly',  id: 1  },
+    { username: 'Joel',   id: 2  },
+    { username: 'Emily',  id: 3  },
+    { username: 'Molly',  id: 4  }
+  ]
+}
