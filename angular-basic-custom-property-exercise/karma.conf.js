a3d70856fd6cc5fdc373e8c09e91bbed3b6b5c32
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-31 15:09:28 -0500

    Exercise 1 and 2

--- angular-basic-custom-property-exercise/karma.conf.js
+++ angular-basic-custom-property-exercise/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/create-nested-components'),
+      dir: require('path').join(__dirname, './coverage/angular-basic-custom-property-exercise'),
       subdir: '.',
       reporters: [
         { type: 'html' },
